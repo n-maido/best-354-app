038c1c9be72af6064dfb311b7c30a272ab60bd5a
n-maido <nhi_maido@outlook.com>
2022-07-19 23:59:37 -0700

    implement routes for rest of the tables

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ app.get('/', async (req, res) => {
 
     // relations.push({ name: "Blood Tested", data: await getTableData("testblood") }); //missing
 
-    relations.push({ name: "Donations (Donors)", data: await getTableData("donate_donor") });
+    relations.push({ name: "Donations-Donors", data: await getTableData("donate_donor") });
 
-    relations.push({ name: "Donations (Blood)", data: await getTableData("donate_blood") });
+    relations.push({ name: "Donations-Blood", data: await getTableData("donate_blood") });
 
     relations.push({ name: "Blood Transported From Blood Drive to Blood Bank", data: await getTableData("transporttobloodbank") });
 
@@ -89,10 +89,181 @@ app.get('/', async (req, res) => {
   
 })
 
-// individual table page
+// individual table pages
 app.get('/Blood-Banks', async (req, res) => {
   try {
     const data = { name: "Blood Banks", data: await getTableData("blood_bank") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Blood-Drives', async (req, res) => {
+  try {
+    const data = { name: "Blood Drives", data: await getTableData("blood_drive") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Hospitals', async (req, res) => {
+  try {
+    const data ={ name: "Hospitals", data: await getTableData("hospital") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Phlebotomists', async (req, res) => {
+  try {
+    const data = { name: "Phlebotomists", data: await getTableData("phlebotomist") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Blood-Bags', async (req, res) => {
+  try {
+    const data = { name: "Blood Bags", data: await getTableData("blood_bag") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Red-Blood-Cells', async (req, res) => {
+  try {
+    const data = { name: "Red Blood Cells", data: await getTableData("red_blood_cells") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Plasma', async (req, res) => {
+  try {
+    const data = { name: "Plasma", data: await getTableData("plasma") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Platelets', async (req, res) => {
+  try {
+    const data = { name: "Platelets", data: await getTableData("platelets") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+// app.get('/Blood-Tested', async (req, res) => {
+//   try {
+//     const data = { name: "Blood Tested", data: await getTableData("testblood") }
+//     res.render('pages/table', data)
+//   } catch (error) {
+//     res.send(error)
+//   }
+// })
+
+app.get('/Donations-Donors', async (req, res) => {
+  try {
+    const data = { name: "Donations-Donors", data: await getTableData("donate_donor") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Donations-Blood', async (req, res) => {
+  try {
+    const data = { name: "Donations-Blood", data: await getTableData("donate_blood") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Blood-Transported-From-Blood-Drive-to-Blood-Bank', async (req, res) => {
+  try {
+    const data = { name: "Blood Transported From Blood Drive to Blood Bank", data: await getTableData("transporttobloodbank") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Blood-Transported-From-Blood-Bank-to-Hospital', async (req, res) => {
+  try {
+    const data = { name: "Blood Transported From Blood Bank to Hospital", data: await getTableData("transporttohospital") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+// app.get('/Blood-Disposed', async (req, res) => {
+//   try {
+//     const data = { name: "Blood Disposed", data: await getTableData("disposeblood") }
+//     res.render('pages/table', data)
+//   } catch (error) {
+//     res.send(error)
+//   }
+// })
+
+// app.get('/Transfusions', async (req, res) => {
+//   try {
+//     const data = { name: "Transfusions", data: await getTableData("transfusion") }
+//     res.render('pages/table', data)
+//   } catch (error) {
+//     res.send(error)
+//   }
+// })
+
+app.get('/Volunteers', async (req, res) => {
+  try {
+    const data = { name: "Volunteers", data: await getTableData("volunteer") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Donors', async (req, res) => {
+  try {
+    const data = { name: "Donors", data: await getTableData("donor") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Donor-Contact', async (req, res) => {
+  try {
+    const data = { name: "Donor Contact", data: await getTableData("donor_contact") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Recipients', async (req, res) => {
+  try {
+    const data = { name: "Recipients", data: await getTableData("recipient") }
+    res.render('pages/table', data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+app.get('/Conducted-Questionnaires', async (req, res) => {
+  try {
+    const data = { name: "Conducted Questionnaires", data: await getTableData("conduct_questionnaire") }
     res.render('pages/table', data)
   } catch (error) {
     res.send(error)
