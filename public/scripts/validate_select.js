c89acda8842c35ce0545319390077d8c9ae35b57
n-maido <nhi_maido@outlook.com>
2022-07-25 19:17:42 -0700

    test conditional input rendering

--- public/scripts/validate_select.js
+++ public/scripts/validate_select.js
@@ -1,20 +1,33 @@
 
-// Validate the form inputs for searching donor_contact
-function validateInputs(form) {
-    let alert = false;
-    let alert_msg = "An input is missing for: \n";
-    let missingFields = [];
+// // Validate the form inputs for searching donor_contact
+// function validateInputs(form) {
+//     let alert = false;
+//     let alert_msg = "An input is missing for: \n";
+//     let missingFields = [];
 
-    console.log(form.value.value);
-    if (form.value.value === "") {
-        console.log("empty")
-        alert = true;
-        missingFields.push("Name");
-        alert_msg += "Name\n";
-    }
-    if (alert) {
-        window.alert(alert_msg);
-    } else {
-        form.submit();
-    }
-}
+//     console.log(form.value.value);
+//     if (form.value.value === "") {
+//         console.log("empty")
+//         alert = true;
+//         missingFields.push("Name");
+//         alert_msg += "Name\n";
+//     }
+//     if (alert) {
+//         window.alert(alert_msg);
+//     } else {
+//         form.submit();
+//     }
+// }
+
+// function generateInput(form) {
+//     console.log(form.field.value)
+  
+//     switch (form.field.value) {
+//         case "email":
+//             return "color"
+//             break
+//         default:
+//             return "text"
+//     }
+    
+// }
